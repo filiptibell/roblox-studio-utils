d4c9456ac41fe335a9700d6ebc751b62fd8fc609
Filip Tibell <filip.tibell@gmail.com>
2025-04-18 13:28:31 +0200

    Get rid of thiserror dependency

--- lib/result.rs
+++ lib/result.rs
@@ -1,27 +1,56 @@
-use std::path::PathBuf;
-
-use thiserror::Error;
+use std::{error::Error, fmt, io, path::PathBuf};
 
 /**
     An error that may occur when interacting with Roblox Studio.
 */
-#[derive(Debug, Error)]
+#[derive(Debug)]
 #[non_exhaustive]
 pub enum RobloxStudioError {
-    #[error("Unknown task: {0}")]
     UnknownTask(String),
-    #[error("Failed to find user documents directory")]
     UserDocumentsDirMissing,
-    #[error("Failed to find local data directory")]
     LocalDataDirMissing,
-    #[error("Failed to copy place file to local data directory: {0}")]
     LocalDataDirCopyPlace(String),
-    #[error("Failed to canonicalize path: {0}")]
     PathCanonicalize(String),
-    #[error("Failed to convert path to string: {0:?}")]
     PathToString(PathBuf),
-    #[error("IO error: {0}")]
-    Io(#[from] std::io::Error),
+    Io(io::Error),
+}
+
+impl fmt::Display for RobloxStudioError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RobloxStudioError::UnknownTask(s) => write!(f, "Unknown task: {s}"),
+            RobloxStudioError::UserDocumentsDirMissing => {
+                write!(f, "Failed to find user documents directory")
+            }
+            RobloxStudioError::LocalDataDirMissing => {
+                write!(f, "Failed to find local data directory")
+            }
+            RobloxStudioError::LocalDataDirCopyPlace(s) => {
+                write!(f, "Failed to copy place file to local data directory: {s}")
+            }
+            RobloxStudioError::PathCanonicalize(s) => write!(f, "Failed to canonicalize path: {s}"),
+            RobloxStudioError::PathToString(p) => {
+                write!(f, "Failed to convert path to string: {p:?}")
+            }
+            RobloxStudioError::Io(e) => write!(f, "I/O error: {e}"),
+        }
+    }
+}
+
+impl Error for RobloxStudioError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        if let Self::Io(e) = self {
+            Some(e)
+        } else {
+            None
+        }
+    }
+}
+
+impl From<io::Error> for RobloxStudioError {
+    fn from(value: io::Error) -> Self {
+        RobloxStudioError::Io(value)
+    }
 }
 
 /**
